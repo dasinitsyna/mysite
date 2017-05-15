8af02004784ecc40064bb02cc6630a2de50fff47
estiuclouds <chocolatelazy@gmail.com>
2017-05-15 11:48:45 +0300

    Add files via upload

---
+++ project4/script.js
@@ -0,0 +1,104 @@
+function c0( )
+{
+   if (c.cycles0.value==='216125')
+     c.resc0.src="img/plus.png";
+   else
+     c.resc0.src="img/minus.png";
+}
+
+function c1( )
+{
+   if (c.cycles1.value==='158')
+     c.resc1.src="img/plus.png";
+   else
+     c.resc1.src="img/minus.png";
+}
+
+function c2( )
+{
+   if (c.cycles2.value==='Preventive')
+     c.resc2.src="img/plus.png";
+   else
+     c.resc2.src="img/minus.png";
+}
+
+function c3( )
+{
+   if (c.cycles3.value==='29')
+     c.resc3.src="img/plus.png";
+   else
+     c.resc3.src="img/minus.png";
+}
+
+function c40( )
+{
+   if (c.cycles40.value==='1254' && c.cycles401.value==='473')
+     c.resc40.src="img/plus.png";
+   else
+     c.resc40.src="img/minus.png";
+}
+
+function c41( )
+{
+   if (c.cycles41.value==='700' && c.cycles411.value==='155')
+     c.resc41.src="img/plus.png";
+   else
+     c.resc41.src="img/minus.png";
+}
+
+function c5( )
+{
+   if (c.cycles5.value==='1 0 0 0 0 0 1 1 0 0 0 1 1 0 1 0')
+     c.resc5.src="img/plus.png";
+   else
+     c.resc5.src="img/minus.png";
+}
+
+function c6( )
+{
+   if (c.cycles6.value==='the')
+     c.resc6.src="img/plus.png";
+   else
+     c.resc6.src="img/minus.png";
+}
+
+function c7( )
+{
+   if (c.cycles7.value==='0')
+     c.resc7.src="img/plus.png";
+   else
+     c.resc7.src="img/minus.png";
+}
+
+function c8( )
+{
+   if (c.cycles8.value==='351')
+     c.resc8.src="img/plus.png";
+   else
+     c.resc8.src="img/minus.png";
+}
+
+function c9( )
+{
+   if (c.cycles9.value==='177')
+     c.resc9.src="img/plus.png";
+   else
+     c.resc9.src="img/minus.png";
+}
+
+function c10( )
+{
+   if (c.cycles10.value==='A linguistic system is a series of differences of sound combined with a series of differences of ideas. Ferdinand de Saussure')
+     c.resc10.src="img/plus.png";
+   else
+     c.resc10.src="img/minus.png";
+}
+
+
+
+
+
+
+
+
+
